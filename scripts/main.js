e00ff77b76d602d60b5eefafea3c9c0cf51e5812
Nik <30575825+NM-tl@users.noreply.github.com>
2022-04-21 18:24:08 +0300

    WeatherApp v.1.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,104 @@
+const KEY = "b98f2d3b7b75f9a4636f70fd1d0ab72c";
+
+const form = document.querySelector(".list-form form");
+const input = document.querySelector(".list-form input");
+const msg = document.querySelector(".list-form .msg");
+const list = document.querySelector(".ajax-section .cities");
+
 function mode () {
     let el = document.body;
     let listBg = document.getElementById('window-list');
     let dataBg = document.getElementById('window-data');
     let dataInfoBg = document.getElementById('data-info');
+    let dataInput = document.getElementById('data-input')
 
     el.classList.toggle('light-mode');
     listBg.classList.toggle('window-list_light');
     dataBg.classList.toggle('window-data_light');
     dataInfoBg.classList.toggle('data-info_light');
-}
+    dataInput.classList.toggle('data-input_light')
+};
 
-function currtime () {
-    let date = new Date();
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
+(function currTime () {
+    Date.prototype.timeNow = function () {
+        return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes();
+    }
+
+    let time =  new Date().timeNow();
     let currTime = document.querySelector('.data-info_time')
 
-    return currTime.innerHTML = `${hours}:${minutes}`;
-}
+    return currTime.innerHTML = time;
+})();
 
-currtime();
-
-const apiKey = "b98f2d3b7b75f9a4636f70fd1d0ab72c";
-const city = "Kyiv";
-
-
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const { main, name, weather} = data;
-        console.log(weather[0].icon);
-        console.log(main);
-
-        let currTemp = document.querySelector('.data-weather_value')
-        currTemp.innerHTML = main.temp.toFixed(0);
-
-        let locationIcon = document.querySelector('.data-weather_icon');
-        const icon = weather[0].icon;
-        locationIcon.innerHTML = `<img class="weather_icon" src="assets/weather/${icon}.png">`;
-
-        let currCity = document.querySelector('.data-info_city')
-        currCity.innerHTML = name;
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let inputVal = "" ? "kyiv" : input.value;
 
 
-    })
-    .catch(() => {
-        alert("Please search for a valid city 😩");
-    });
+    const listItems = list.querySelectorAll(".ajax-section .city");
+    const listItemsArray = Array.from(listItems);
 
+    if (listItemsArray.length > 0) {
+        const filteredArray = listItemsArray.filter(el => {
+            let content = "";
+            if (inputVal.includes(",")) {
+                if (inputVal.split(",")[1].length > 2) {
+                    inputVal = inputVal.split(",")[0];
+                } else {
+                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
+                }
+            } else {
+                //athens
+                content = el.querySelector(".city-name span").textContent.toLowerCase();
+            }
+            return content == inputVal.toLowerCase();
+        });
 
+        if (filteredArray.length > 0) {
+            msg.textContent = `You already know the weather for ${
+                filteredArray[0].querySelector(".city-name span").textContent
+            } ...otherwise be more specific by providing the country code as well 😉`;
+            form.reset();
+            input.focus();
+            return;
+        }
+    }
 
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${KEY}&units=metric`;
 
-// let geolocationRequest = new XMLHttpRequest();
-// geolocationRequest.open('GET', 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=49.6009216&longitude=34.5014272&localityLanguage=en')
-// geolocationRequest.responseType = 'text';
-//
-// console.log(geolocationRequest);
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            const { main, name, sys, weather, wind} = data;
 
+            let currTemp = document.querySelector('.data-weather_value')
+            currTemp.innerHTML = `${Math.round(main.temp)}<sup>°C</sup>`;
 
+            let locationIcon = document.querySelector('.data-weather_icon');
+            locationIcon.innerHTML = `<img class="weather_icon" src="assets/weather/${weather[0]["icon"]}.png">`;
 
+            let locationCaption = document.querySelector('.data-weather_caption');
+            locationCaption.innerHTML = `${weather[0]["description"]}`
 
+            let currCity = document.querySelector('.data-info_city')
+            currCity.innerHTML = name;
 
+            let currCountry = document.querySelector('.data-info_country')
+            currCountry.innerHTML = sys.country;
 
-// 49.6009216
-// 34.5014272
-// const API = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${}&localityLanguage=en`;
+            let currWindSpeed = document.querySelector('.wind-speed')
+            currWindSpeed.innerHTML = `Wind speed: ${wind.speed}`;
+
+            let currWindDeg = document.querySelector('.wind-deg')
+            currWindDeg.innerHTML = `Wind Degree Direction: ${wind.deg}`;
+
+        })
+        .catch(() => {
+            msg.textContent = "Please search for a valid city 😩";
+        });
+
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+});
+
